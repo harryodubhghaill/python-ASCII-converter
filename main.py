aedb0b5b536bc10ff3f43df1dfd559edc04e6423
Harry Doyle <harryodubhghaill@hotmail.com>
2023-01-10 21:25:40 +0000

    add ArtConverter class skeleton

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame as pg
+import cv2
+
+
+class ArtConverter:
+  def __init__(self):
+    pass
+
+  def get_image(self):
+    pass
+
+  def draw(self):
+    pass
+
+  def run(self):
+    pass
+
+
+if __name__ = '__main__':
+  app = ArtConverter()
+  app.run()
